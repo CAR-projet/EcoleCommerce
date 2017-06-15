19fc047960d7128c547a6247715ae6c3e5b1d13a
Unknown <c.boineau@live.fr>
2017-06-15 14:36:31 +0200

    Question Proposition

--- src/fr/sopra/model/Proposition.java
+++ src/fr/sopra/model/Proposition.java
@@ -1,9 +1,82 @@
 package fr.sopra.model;
 
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name="proposition", uniqueConstraints = { @UniqueConstraint(columnNames = "UTI_USERNAME") })
 public class Proposition {
 
-	public Proposition() {
-		// TODO Auto-generated constructor stub
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="PRO_ID")
+	private Integer idProposition;
+	
+	@Column(name="PRO_COR")
+	@NotBlank
+	private Boolean isCorrect;
+	
+	@Column(name="PRO_NUM")
+	@NotBlank
+	private char numeroProposition;
+	
+	
+
+	public Integer getIdProposition() {
+		return idProposition;
+	}
+
+
+
+
+	public void setIdProposition(Integer idProposition) {
+		this.idProposition = idProposition;
+	}
+
+
+
+
+	public Boolean getIsCorrect() {
+		return isCorrect;
+	}
+
+
+
+
+	public void setIsCorrect(Boolean isCorrect) {
+		this.isCorrect = isCorrect;
+	}
+
+
+
+
+	public char getNumeroProposition() {
+		return numeroProposition;
+	}
+
+
+
+
+	public void setNumeroProposition(char numeroProposition) {
+		this.numeroProposition = numeroProposition;
+	}
+
+
+
+
+	public Proposition() {	
+		
 	}
 
 }
--- src/fr/sopra/model/Question.java
+++ src/fr/sopra/model/Question.java
@@ -1,9 +1,74 @@
 package fr.sopra.model;
 
-public class Question {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	public Question() {
-		// TODO Auto-generated constructor stub
+import org.hibernate.validator.constraints.NotBlank;
+
+
+@Entity
+@Table(name="question")
+public class Question {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="QUESTO_ID")
+	private Integer idQuestion;
+	
+	
+	@Column(name="QUESTO_ID")
+	@NotBlank
+	private String intitule;
+	
+	
+	@Column(name="QUESTO_ID")
+	@NotBlank
+	private Integer numeroQuestion;
+	
+	
+	
+	
+	public Integer getIdQuestion() {
+		return idQuestion;
 	}
 
+
+
+	public void setIdQuestion(Integer idQuestion) {
+		this.idQuestion = idQuestion;
+	}
+
+
+
+	public String getIntitule() {
+		return intitule;
+	}
+
+
+
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+
+
+
+	public Integer getNumeroQuestion() {
+		return numeroQuestion;
+	}
+
+
+
+	public void setNumeroQuestion(Integer numeroQuestion) {
+		this.numeroQuestion = numeroQuestion;
+	}
+
+
+	public Question() {}
+
+	
 }
