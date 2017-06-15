85890bf9e1b4dfa0b73532e2f3ea08752f149654
anaislancry <anaislancry@yahoo.fr>
2017-06-15 14:04:41 +0200

    Creation Classes

---
+++ src/fr/sopra/model/Matiere.java
@@ -0,0 +1,9 @@
+package fr.sopra.model;
+
+public class Matiere {
+
+	public Matiere() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/fr/sopra/model/Questionnaire.java
@@ -0,0 +1,9 @@
+package fr.sopra.model;
+
+public class Questionnaire {
+
+	public Questionnaire() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
